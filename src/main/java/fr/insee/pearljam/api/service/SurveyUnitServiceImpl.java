180b3d9d2376241aedfb7f83ce70226b2308ea51
paul guillemet <paul.guillemet@keyconsulting.fr>
2020-09-04 12:59:15 +0000

    Merged PR 236: fix: removing control
    fix: removing control

--- src/main/java/fr/insee/pearljam/api/service/SurveyUnitServiceImpl.java
+++ src/main/java/fr/insee/pearljam/api/service/SurveyUnitServiceImpl.java
@@ -162,30 +162,31 @@ public class SurveyUnitServiceImpl implements SurveyUnitService {
 			return HttpStatus.BAD_REQUEST;
 		}
 		
-		List<StateDto> sortedStates = surveyUnitDetailDto.getStates();
-		Collections.sort(sortedStates, new Comparator<StateDto>() {
-		    @Override
-		    public int compare(StateDto o1, StateDto o2) {
-		        return o1.getDate().compareTo(o2.getDate());
-		    }
-		});
-		StateDto prevState = null;
-		Boolean wasAlreadyPresent = true;
-		for(StateDto state : sortedStates) {
-			if(wasAlreadyPresent) {
-				Optional<StateDto> s = stateRepository.findDtoById(state.getId());
-				if(!s.isPresent() || !s.get().getType().equals(state.getType()) 
-					|| !s.get().getDate().equals(state.getDate())) {
-					wasAlreadyPresent = false;
-				}
-			}
-			if(!wasAlreadyPresent && (prevState == null
-				|| !BussinessRules.stateCanBeModifiedByInterviewer(prevState.getType(), state.getType()))) {
-				LOGGER.error("State modifications requested are not allowed");
-				return HttpStatus.FORBIDDEN;
-			}
-			prevState = state;
-		}
+		// List<StateDto> sortedStates = surveyUnitDetailDto.getStates();
+		// Collections.sort(sortedStates, new Comparator<StateDto>() {
+		//     @Override
+		//     public int compare(StateDto o1, StateDto o2) {
+		//         return o1.getDate().compareTo(o2.getDate());
+		//     }
+		// });
+		
+		//StateDto prevState = null;
+		//Boolean wasAlreadyPresent = true;
+		//for(StateDto state : sortedStates) {
+		//	if(wasAlreadyPresent) {
+		//		Optional<StateDto> s = stateRepository.findDtoById(state.getId());
+		//		if(!s.isPresent() || !s.get().getType().equals(state.getType()) 
+		//			|| !s.get().getDate().equals(state.getDate())) {
+		//			wasAlreadyPresent = false;
+		//		}
+		//	}
+		//	if(!wasAlreadyPresent && (prevState == null
+		//		|| !BussinessRules.stateCanBeModifiedByInterviewer(prevState.getType(), state.getType()))) {
+		//		LOGGER.error("State modifications requested are not allowed");
+		//		return HttpStatus.FORBIDDEN;
+		//	}
+		//	prevState = state;
+		//}
 		
 		Optional<SurveyUnit> surveyUnit = null;
 		if(userId.equals(GUEST)) {
