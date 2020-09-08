213f23988e1d1ce8a8e5fa68da8da1d6346d7aef
Paul Guillemet <paul.guillemet@keyconsulting.fr>
2020-09-08 10:45:13 +0200

    feat: endpoint tests

--- src/main/java/fr/insee/pearljam/api/dto/visibility/VisibilityDto.java
+++ src/main/java/fr/insee/pearljam/api/dto/visibility/VisibilityDto.java
@@ -25,6 +25,10 @@ public class VisibilityDto {
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
+	
+	public VisibilityDto() {
+		super();
+	}
 
 	public Long getStartDate() {
 		return startDate;
