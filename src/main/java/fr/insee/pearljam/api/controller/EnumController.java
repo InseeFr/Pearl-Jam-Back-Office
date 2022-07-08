e67cab91dc849dc2bf527b2c9a897795de31aec7
Simon Demazière <simon.demaziere@insee.fr>
2022-07-08 14:08:53 +0200

    expose enums over api

---
+++ src/main/java/fr/insee/pearljam/api/controller/EnumController.java
@@ -0,0 +1,45 @@
+package fr.insee.pearljam.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.insee.pearljam.api.domain.ContactOutcomeType;
+import fr.insee.pearljam.api.domain.StateType;
+import fr.insee.pearljam.api.domain.Status;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping(path = "/api")
+public class EnumController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnumController.class);
+
+    @ApiOperation(value = "Get enum")
+    @GetMapping(path = "/enum/state")
+    public ResponseEntity<Object> getStateEnum() {
+
+        StateType[] enumValues = StateType.values();
+        return new ResponseEntity<Object>(enumValues, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Get enum")
+    @GetMapping(path = "/enum/contactAttempt")
+    public ResponseEntity<Object> getContactAttemptEnum() {
+
+        Status[] enumValues = Status.values();
+        return new ResponseEntity<Object>(enumValues, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Get enum")
+    @GetMapping(path = "/enum/contactOutcome")
+    public ResponseEntity<Object> getContactOutcomeEnum() {
+
+        ContactOutcomeType[] enumValues = ContactOutcomeType.values();
+        return new ResponseEntity<Object>(enumValues, HttpStatus.OK);
+    }
+
+}
