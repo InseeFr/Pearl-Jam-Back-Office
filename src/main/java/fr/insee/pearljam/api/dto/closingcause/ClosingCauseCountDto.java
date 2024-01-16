a2689a0ebc1ed443f4bbf6a14c970f4fc8aea7ed
Simon Demaziere <simon.demaziere@insee.fr>
2024-01-16 11:20:32 +0100

    fix: revert field mapping code

--- src/main/java/fr/insee/pearljam/api/dto/closingcause/ClosingCauseCountDto.java
+++ src/main/java/fr/insee/pearljam/api/dto/closingcause/ClosingCauseCountDto.java
@@ -1,17 +1,20 @@
 package fr.insee.pearljam.api.dto.closingcause;
 
+import java.lang.reflect.Field;
 import java.math.BigInteger;
 import java.util.Map;
+import java.util.Optional;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import static fr.insee.pearljam.api.constants.Constants.*;
-
+import fr.insee.pearljam.api.constants.Constants;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
+@Slf4j
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ClosingCauseCountDto {
 	private Long npiCount;
@@ -20,50 +23,29 @@ public class ClosingCauseCountDto {
 	private Long rowCount;
 	private Long total;
 
-	private void initializeFields() {
-		for (String fieldName : CLOSING_CAUSE_FIELDS) {
-			setFieldValue(fieldName, 0L);
-		}
-	}
-
-	private void setFieldValue(String fieldName, Long value) {
-		switch (fieldName) {
-			case NPI_COUNT:
-				npiCount = value;
-				break;
-			case NPA_COUNT:
-				npaCount = value;
-				break;
-			case NPX_COUNT:
-				npxCount = value;
-				break;
-			case ROW_COUNT:
-				rowCount = value;
-				break;
-			case TOTAL_COUNT:
-				total = value;
-				break;
-			// Add more cases for other fields if needed
-			default:
-				// Handle unknown field name
-				break;
-		}
-	}
-
 	public ClosingCauseCountDto() {
 		super();
 	}
 
 	public ClosingCauseCountDto(Map<String, BigInteger> obj) {
-		initializeFields();
-		if (obj != null) {
-			for (Map.Entry<String, BigInteger> entry : obj.entrySet()) {
-				String fieldName = entry.getKey();
-				Long value = entry.getValue().longValue();
-				setFieldValue(fieldName, value);
+		boolean nullOrEmpty = Optional.ofNullable(obj.isEmpty()).orElse(true);
+
+		for (String str : Constants.CLOSING_CAUSE_FIELDS) {
+			if (nullOrEmpty) {
+				setLongField(str, 0L);
+			} else {
+				setLongField(str, Optional.ofNullable(obj.get(str)).orElse(BigInteger.ZERO).longValue());
 			}
 		}
+	}
 
+	public void setLongField(String fieldName, Long value) {
+		try {
+			Field field = getClass().getDeclaredField(fieldName);
+			field.set(this, value);
+		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			log.warn("Couldn't set field {} with value", fieldName, value);
+		}
 	}
 
 }
--- src/main/java/fr/insee/pearljam/api/dto/closingcause/ClosingCauseDto.java
+++ src/main/java/fr/insee/pearljam/api/dto/closingcause/ClosingCauseDto.java
@@ -1,7 +1,11 @@
 package fr.insee.pearljam.api.dto.closingcause;
 
 import fr.insee.pearljam.api.domain.ClosingCauseType;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ClosingCauseDto {
 	/**
 	 * the date of ContactOutcome
@@ -11,37 +15,15 @@ public class ClosingCauseDto {
 	 * the OutcomeType of ContactOutcome
 	 */
 	private ClosingCauseType type;
+
 	public ClosingCauseDto(Long date, ClosingCauseType type) {
 		super();
 		this.date = date;
 		this.type = type;
 	}
+
 	public ClosingCauseDto() {
 		super();
-	}
-	/**
-	 * @return the date
-	 */
-	public Long getDate() {
-		return date;
-	}
-	/**
-	 * @param date the date to set
-	 */
-	public void setDate(Long date) {
-		this.date = date;
-	}
-	/**
-	 * @return the type
-	 */
-	public ClosingCauseType getType() {
-		return type;
-	}
-	/**
-	 * @param type the type to set
-	 */
-	public void setType(ClosingCauseType type) {
-		this.type = type;
 	}
 
 }
