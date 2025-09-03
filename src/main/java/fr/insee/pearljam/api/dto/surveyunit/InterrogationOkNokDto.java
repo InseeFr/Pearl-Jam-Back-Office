7f86f76f2673f4532e0e01bb337d8644b070fd11
PGPU0I <camille.corbin@insee.fr>
2025-09-03 14:09:55 +0200

    fix: use record for InterrogationOkNOk

--- src/main/java/fr/insee/pearljam/api/dto/surveyunit/InterrogationOkNokDto.java
+++ src/main/java/fr/insee/pearljam/api/dto/surveyunit/InterrogationOkNokDto.java
@@ -1,29 +1,15 @@
 package fr.insee.pearljam.api.dto.surveyunit;
 
-
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class InterrogationOkNokDto {
+public record InterrogationOkNokDto(
+    List<InterrogationOkNokResponseDto> interrogationOK,
+    List<InterrogationOkNokResponseDto> interrogationNOK
+) {
 
-  List<InterrogationOkNokResponseDto> interrogationOK;
-  List<InterrogationOkNokResponseDto> interrogationNOK;
-	
-	public InterrogationOkNokDto(List<InterrogationOkNokResponseDto> interrogationOK, List<InterrogationOkNokResponseDto> interrogationNOK) {
-		super();
-		this.interrogationOK = interrogationOK;
-		this.interrogationNOK = interrogationNOK;
-	}
-
-	public InterrogationOkNokDto() {
-		super();
-	}
-
-
+  public InterrogationOkNokDto() {
+    this(null, null);
+  }
 }
--- src/main/java/fr/insee/pearljam/api/dto/surveyunit/InterrogationOkNokResponseDto.java
+++ src/main/java/fr/insee/pearljam/api/dto/surveyunit/InterrogationOkNokResponseDto.java
@@ -1,31 +1,19 @@
 package fr.insee.pearljam.api.dto.surveyunit;
 
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import fr.insee.pearljam.api.dto.statedata.StateDataDto;
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class InterrogationOkNokResponseDto {
-	String id;
-	private StateDataDto stateData;
-	
-	public InterrogationOkNokResponseDto() {
-		super();
-	}
+public record InterrogationOkNokResponseDto(
+    String id,
+    StateDataDto stateData
+)
+{
+  public InterrogationOkNokResponseDto() {
+    this(null, null);
+  }
 
-	public InterrogationOkNokResponseDto(String id, StateDataDto stateData) {
-		super();
-		this.id = id;
-		this.stateData = stateData;
-	}
-	public InterrogationOkNokResponseDto(String id) {
-		super();
-		this.id = id;
-	}
-
+  public InterrogationOkNokResponseDto(String id) {
+    this(id, null);
+  }
 }
