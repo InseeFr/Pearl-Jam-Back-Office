6171eb5f7e6ba0e17bf5a0c848966d09dd84babd
Pierre-Raphael Wozny <pierre-raphael.wozny@insee.fr>
2026-01-13 17:24:36 +0100

    test: e2e data fix

--- src/main/resources/db/dataset/init-demo-data.sql
+++ src/main/resources/db/dataset/init-demo-data.sql
@@ -605,7 +605,9 @@ INSERT INTO public.phone_number (id, favorite, number, source, person_id) VALUES
     (75, TRUE, '+33677542866', 0, 60),
     (76, TRUE, '+33677542866', 0, 61),
     (77, TRUE, '+33778899110', 0, 69),
-    (78, TRUE, '+33778899112', 0, 70);
+    (78, TRUE, '+33778899112', 0, 70),
+    (79, TRUE, '+33771755198', 0, 74);
+
 
 
 
