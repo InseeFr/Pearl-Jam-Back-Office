667f0a1b3f4887216591a1032f7dc9a291836b8c
Pierre-Raphael Wozny <pierre-raphael.wozny@insee.fr>
2026-03-23 16:46:42 +0100

    fix: removed unused jpa repository

--- pearljam-infrastructure-persistence/src/main/java/fr/insee/pearljam/infrastructure/persistence/reporting/jpa/CampaignProgressionJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.insee.pearljam.infrastructure.persistence.reporting.jpa;
-
-import fr.insee.pearljam.infrastructure.persistence.organizationunit.entity.UserDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampaignProgressionJpaRepository  extends JpaRepository<UserDB, String> {
-}
