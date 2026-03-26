86417ebd8cab1ded4f44d51c527cf50932f4384b
Pierre-Raphael Wozny <pierre-raphael.wozny@insee.fr>
2026-03-26 15:23:07 +0100

    fix: removed unused method

--- pearljam-domain-model/src/main/java/fr/insee/pearljam/domain/surveyunit/model/count/CommunicationRequestCount.java
+++ pearljam-domain-model/src/main/java/fr/insee/pearljam/domain/surveyunit/model/count/CommunicationRequestCount.java
@@ -4,8 +4,4 @@ public record CommunicationRequestCount(
         String entityId,
         Long noticeCount,
         Long reminderCount) {
-    
-    public static CommunicationRequestCount empty(String id) {
-        return new CommunicationRequestCount(id, 0L, 0L);
-    }
 }
