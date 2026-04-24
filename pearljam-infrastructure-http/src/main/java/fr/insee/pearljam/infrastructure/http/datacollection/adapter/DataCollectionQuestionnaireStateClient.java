43cccd783bd672c2f00764f3874431ec72e6e910
Betty Becuwe <betty.becuwe@insee.fr>
2026-04-24 10:36:24 +0200

    refactor: remove useless request

--- pearljam-infrastructure-http/src/main/java/fr/insee/pearljam/infrastructure/http/datacollection/adapter/DataCollectionQuestionnaireStateClient.java
+++ pearljam-infrastructure-http/src/main/java/fr/insee/pearljam/infrastructure/http/datacollection/adapter/DataCollectionQuestionnaireStateClient.java
@@ -1,12 +1,13 @@
 package fr.insee.pearljam.infrastructure.http.datacollection.adapter;
 
-import fr.insee.pearljam.contracts.constants.Constants;
 import fr.insee.pearljam.contracts.surveyunit.dto.surveyunit.InterrogationOkNokDto;
+import fr.insee.pearljam.contracts.constants.Constants;
 import fr.insee.pearljam.domain.surveyunit.port.out.QuestionnaireStateClient;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -17,20 +18,18 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class DataCollectionQuestionnaireStateClient implements QuestionnaireStateClient {
 
-    @Qualifier("dataCollectionRestClient")
-    private final RestClient.Builder dataCollectionRestClient;
+	@Qualifier("dataCollectionRestClient")
+	private final RestClient.Builder dataCollectionRestClient;
 
-    @Override
-    public ResponseEntity<InterrogationOkNokDto> getQuestionnairesStateFromDataCollection(
-            Set<String> ids) {
-        return dataCollectionRestClient
-                .build()
-                .post()
-                .uri(Constants.API_QUEEN_INTERROGATIONS_STATEDATA)
-                .body(ids)
-                .retrieve()
-                .toEntity(InterrogationOkNokDto.class);
-    }
-
-
+	@Override
+	public ResponseEntity<InterrogationOkNokDto> getQuestionnairesStateFromDataCollection(
+			Set<String> ids) {
+		return dataCollectionRestClient
+				.build()
+				.post()
+				.uri(Constants.API_QUEEN_INTERROGATIONS_STATEDATA)
+				.body(ids)
+				.retrieve()
+				.toEntity(InterrogationOkNokDto.class);
+	}
 }
