382dcd958a407725df3884bcfad7e753c56486d7
Betty Becuwe <betty.becuwe@insee.fr>
2026-04-24 11:00:17 +0200

    refactor: add component to presenter

--- pearljam-api/src/main/java/fr/insee/pearljam/api/surveyunit/presenter/SurveyUnitToClosePresenter.java
+++ pearljam-api/src/main/java/fr/insee/pearljam/api/surveyunit/presenter/SurveyUnitToClosePresenter.java
@@ -4,10 +4,12 @@ import fr.insee.pearljam.api.surveyunit.response.SurveyUnitToCloseResponse;
 import fr.insee.pearljam.domain.surveyunit.port.in.SurveyUnitToCloseStatsPresenter;
 import fr.insee.pearljam.domain.surveyunit.port.out.view.ClosableSurveyUnitCandidateView;
 import fr.insee.pearljam.domain.surveyunit.port.out.view.ClosableSurveyUnitView;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class SurveyUnitToClosePresenter implements SurveyUnitToCloseStatsPresenter<List<SurveyUnitToCloseResponse>> {
 
     private static final String QUESTIONNAIRE_STATE_UNAVAILABLE = "UNAVAILABLE";
