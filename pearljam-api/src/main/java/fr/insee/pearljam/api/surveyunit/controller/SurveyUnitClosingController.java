019793a474963805caa27f768c8a4de09d59bd81
Pierre-Raphael Wozny <pierre-raphael.wozny@insee.fr>
2026-04-28 11:46:51 +0200

    feat: add integration test for put

--- pearljam-api/src/main/java/fr/insee/pearljam/api/surveyunit/controller/SurveyUnitClosingController.java
+++ pearljam-api/src/main/java/fr/insee/pearljam/api/surveyunit/controller/SurveyUnitClosingController.java
@@ -1,7 +1,6 @@
 package fr.insee.pearljam.api.surveyunit.controller;
 
 import fr.insee.pearljam.api.surveyunit.controller.request.CloseSurveyUnitsRequest;
-import fr.insee.pearljam.domain.surveyunit.model.closingcause.ClosingCauseType;
 import fr.insee.pearljam.domain.surveyunit.port.in.SurveyUnitClosingPort;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -9,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
