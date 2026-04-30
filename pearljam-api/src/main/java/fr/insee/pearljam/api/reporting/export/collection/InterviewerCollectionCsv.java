511acb7046aaa1dffd93a5662520df479fe83ffa
David Darras <david.darras@insee.fr>
2026-04-30 14:59:50 +0200

    refactor: add presenter for interviewer collection export

--- pearljam-api/src/main/java/fr/insee/pearljam/api/reporting/export/collection/InterviewerCollectionCsv.java
+++ pearljam-api/src/main/java/fr/insee/pearljam/api/reporting/export/collection/InterviewerCollectionCsv.java
@@ -2,9 +2,7 @@ package fr.insee.pearljam.api.reporting.export.collection;
 
 import fr.insee.pearljam.api.reporting.export.csv.CsvExportable;
 import fr.insee.pearljam.api.reporting.export.csv.CsvRow;
-import fr.insee.pearljam.api.reporting.response.CampaignCollectionByInterviewersResponse;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public record InterviewerCollectionCsv(List<CsvRow> rows) implements CsvExportable {
@@ -12,21 +10,6 @@ public record InterviewerCollectionCsv(List<CsvRow> rows) implements CsvExportab
     public static final List<CollectionCsvHeaders> CSV_HEADERS = CollectionCsvHeaders.buildHeaders(
             List.of(CollectionCsvHeaders.INTERVIEWER_LABEL, CollectionCsvHeaders.INTERVIEWER_ID)
     );
-
-    public static InterviewerCollectionCsv from(CampaignCollectionByInterviewersResponse response) {
-        List<CsvRow> rows = response.interviewers().stream()
-                .map(interviewer -> {
-                    List<Object> values = new ArrayList<>();
-                    values.add(interviewer.interviewerLabel());
-                    values.add(interviewer.interviewerId());
-                    values.addAll(CollectionCsvRow.commonValues(
-                            interviewer.rates(), interviewer.outcomes(), interviewer.closingCauses(),
-                            interviewer.allocated()));
-                    return CsvRow.from(values.toArray());
-                })
-                .toList();
-        return new InterviewerCollectionCsv(rows);
-    }
 
     @Override
     public CsvRow headers() {
--- pearljam-api/src/main/java/fr/insee/pearljam/api/reporting/export/progress/ProgressCsvRow.java
+++ pearljam-api/src/main/java/fr/insee/pearljam/api/reporting/export/progress/ProgressCsvRow.java
@@ -1,8 +1,5 @@
 package fr.insee.pearljam.api.reporting.export.progress;
 
-import fr.insee.pearljam.api.reporting.response.CommunicationsProgressResponse;
-import fr.insee.pearljam.api.reporting.response.StatesInterviewerProgressResponse;
-import fr.insee.pearljam.api.reporting.response.StatesProgressResponse;
 import fr.insee.pearljam.domain.reporting.readmodel.AbstractDailyStats;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -20,32 +17,6 @@ class ProgressCsvRow {
                 stats.getPrcStateCount(), stats.getAocStateCount(), stats.getApsStateCount(),
                 stats.getInsStateCount(),
                 stats.getNoticeCommunicationCount(), stats.getReminderCommunicationCount()
-        );
-    }
-
-    static List<Object> commonValues(float progressRate,
-                                     StatesProgressResponse states,
-                                     CommunicationsProgressResponse communications) {
-        return List.of(
-                progressRate,
-                states.allocated(), states.notStarted(), states.inProgress(),
-                states.pendingTransmission(), states.toReview(), states.validated(),
-                states.preparingContact(), states.withContact(), states.withAppointment(),
-                states.started(),
-                communications.noticeLetter(), communications.reminderLetter()
-        );
-    }
-
-    static List<Object> commonValues(float progressRate,
-                                     StatesInterviewerProgressResponse states,
-                                     CommunicationsProgressResponse communications) {
-        return List.of(
-                progressRate,
-                states.allocatedInterviewers(), states.notStarted(), states.inProgress(),
-                states.pendingTransmission(), states.toReview(), states.validated(),
-                states.preparingContact(), states.withContact(), states.withAppointment(),
-                states.started(),
-                communications.noticeLetter(), communications.reminderLetter()
         );
     }
 }
