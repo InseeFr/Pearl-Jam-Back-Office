6386f6878eafdf8cdf77893d1c1f6ab76f816bbf
Betty Becuwe <betty.becuwe@insee.fr>
2026-06-03 16:20:30 +0200

    refactor: move classes

--- pearljam-domain/src/main/java/fr/insee/pearljam/domain/surveyunit/service/SurveyUnitHistoryService.java
+++ pearljam-domain/src/main/java/fr/insee/pearljam/domain/surveyunit/service/SurveyUnitHistoryService.java
@@ -2,7 +2,10 @@ package fr.insee.pearljam.domain.surveyunit.service;
 
 import fr.insee.pearljam.domain.surveyunit.port.in.SurveyUnitHistoryPort;
 import fr.insee.pearljam.domain.surveyunit.port.in.SurveyUnitHistoryPresenter;
+import fr.insee.pearljam.domain.surveyunit.port.out.CommunicationRequestRepository;
+import fr.insee.pearljam.domain.surveyunit.port.out.StateRepository;
 import fr.insee.pearljam.domain.surveyunit.port.out.SurveyUnitHistoryRepositoryPort;
+import fr.insee.pearljam.domain.surveyunit.port.out.SurveyUnitRepository;
 import fr.insee.pearljam.domain.surveyunit.readmodel.SurveyUnitHistory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +17,10 @@ import org.springframework.stereotype.Service;
 public class SurveyUnitHistoryService implements SurveyUnitHistoryPort {
 
     private final SurveyUnitHistoryRepositoryPort surveyUnitHistoryRepositoryPort;
+
+    private final SurveyUnitRepository surveyUnitRepository;
+    private final StateRepository stateRepository;
+    private final CommunicationRequestRepository communicationRepository;
 
     @Override
     public <T> T getSurveyUnitHistory(String surveyUnitId, SurveyUnitHistoryPresenter<T> presenter) {
