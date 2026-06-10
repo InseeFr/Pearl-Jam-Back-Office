4c7d3650739cf8099188acdfaf886dc27eff3090
Betty Becuwe <betty.becuwe@insee.fr>
2026-06-10 16:48:11 +0200

    refactor: to close in request

--- pearljam-api/src/main/java/fr/insee/pearljam/api/surveyunit/controller/SurveyUnitClosingController.java
+++ pearljam-api/src/main/java/fr/insee/pearljam/api/surveyunit/controller/SurveyUnitClosingController.java
@@ -14,7 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -33,10 +36,9 @@ public class SurveyUnitClosingController {
      */
     @PostMapping(Constants.API_SURVEYUNIT_CLOSE_SURVEYUNITS)
     public ResponseEntity<Void> addClosingCauseToMultipleSurveyUnits(
-            @RequestBody @Valid CloseSurveyUnitsRequest request,
-            @RequestParam(required = false, defaultValue  ="true") boolean toClose) {
+            @RequestBody @Valid CloseSurveyUnitsRequest request) {
 
-        surveyUnitClosingPort.addClosingCauseToMultipleSurveyUnits(request.getSurveyUnitIds(), request.getClosingCauseType(), toClose);
+        surveyUnitClosingPort.addClosingCauseToMultipleSurveyUnits(request.getSurveyUnitIds(), request.getClosingCauseType(), request.getToClose());
         return ResponseEntity.noContent().build();
     }
 
