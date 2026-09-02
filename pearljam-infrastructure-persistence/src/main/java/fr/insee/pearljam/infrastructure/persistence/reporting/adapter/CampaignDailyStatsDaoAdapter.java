2d5e1b282585795f475cdfe37f78b5b894ed07bb
Pierre-Raphael Wozny <pierre-raphael.wozny@insee.fr>
2026-09-02 15:16:35 +0200

    fix: sonar issue

--- pearljam-infrastructure-persistence/src/main/java/fr/insee/pearljam/infrastructure/persistence/reporting/adapter/CampaignDailyStatsDaoAdapter.java
+++ pearljam-infrastructure-persistence/src/main/java/fr/insee/pearljam/infrastructure/persistence/reporting/adapter/CampaignDailyStatsDaoAdapter.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
@@ -486,7 +487,7 @@ FROM (
                 .param("stateOffset", newState != null ? 1 : 0)
                 .param("updatedAt", updatedAt.toEpochMilli())
                 .param("causeOffset", StateType.CLO.equals(newState) && closingCause != null ? 1 : 0)
-                .param("targetDay", LocalDate.now())
+                .param("targetDay", LocalDate.now(ZoneId.of("UTC")))
                 .update();
     }
 }
